a39ab718d27528804cff4b0df1fd3cc3f41bedb8
Sudip-Creates <sudipbohara502@gmail.com>
2025-11-23 22:45:37 +0545

    Server Optimization

--- config/constants.ts
+++ config/constants.ts
@@ -3,5 +3,5 @@ export const CONFIG = {
     MONGO_URI: "mongodb+srv://sudipbohara502_db_user:Qu8rixdCpr9Bw3r@taskmanager.ociyogc.mongodb.net/?appName=taskManager",
     JWT_SECRET: "GREED123HAHAHA",
     ADMIN_INVITE_TOKEN: "9860989021",
-    CLIENT_URI: "http://localhost:3000",
+    CLIENT_URI: "http://localhost:5173",
 };
